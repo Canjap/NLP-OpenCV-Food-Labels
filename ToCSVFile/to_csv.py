0fc0a493cc1caebd7b7270682c41ec13225d1f8f
Sanjay <sanjaychemban@gmail.com>
2023-04-06 09:33:47 -0400

    tested dict with testing lists

--- ToCSVFile/to_csv.py
+++ ToCSVFile/to_csv.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np 
 import sys 
 sys.path.append("C:\\Users\\sanja\\OneDrive\\Documents\\GitHub\\NLP-OpenCV-Food-Labels\\text_detection") # adds text_detection folder to list of modules/libs Python uses
-import keras_text
 
-words_list = ["apple", "cinammon", "cereal", "Nutrition"]#keras_text.pipelineProcessor()
-nutrion_label = " ".join(words_list).split("Nutrition")
+
+words_list = ["apple", "cinammon", "cereal", "Nutrition", "Carbs", 230, "fats", 25]#keras_text.pipelineProcessor()
+packaging_list = words_list[:words_list.index("Nutrition")]
+nutrition_label = words_list[words_list.index("Nutrition"):]
 data = {
-    "Label" : [words_list],
-    #"Nutrition Label" : 
-
+    "Label" : [packaging_list],
+    "Nutrition Label" : [nutrition_label]
     }
 df = pd.DataFrame(data)
 
