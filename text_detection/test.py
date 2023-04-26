1a73b50552bed832247e7bbd36bf54b947840f0d
Sanjay <sanjaychemban@gmail.com>
2023-04-25 20:38:26 -0400

    wrote test code to search and get nutrition facts of food item

---
+++ text_detection/test.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+r = requests.get(f"https://www.myfooddiary.com/foods/4029949/kelloggs-apple-cinnamon-nutri-grain-bar")
+
+soup = BeautifulSoup(r.content, "html.parser")
+
+divs = soup.find(id = "FoodLabelHldr")
+
+to_df = divs.text
+to_df = to_df.splitlines()
+to_df = [elem for elem in to_df if elem.strip()]
+nono_words = ['Nutrition Facts', 'Amount Per Serving', '% Daily Value*', 'The % Daily Value (DV) tells you how much a nutrient in a serving of food contributes to a daily diet. 2,000 calories a day is used for general nutrition advice.']
+for x in to_df:
+    if x in nono_words:
+        to_df.remove(x)
+to_df[18] = to_df[18].strip()
+to_df[-1] = to_df[-1].strip()
+to_df.remove(to_df[0])
+
+print(to_df)
+for x in to_df:
+    if x != to_df[-1]:
+        if x.endswith('%') or x.endswith("0"):
+            to_df[to_df.index(x)+1] += f" {x}"
+            to_df.remove(x)
+    else:
+        break
+
+name =[]
+mg = []
+percents = []
+for x in to_df:
+    x = x.split()
+    print(x)
